90598b6e542c6be7a01c543f2e8cf09233bd81a5
Jhon Perez <jrperez175@gmail.com>
2018-03-13 15:46:46 -0500

    Commit 13Marzo2018 15:46

--- src/main/java/org/gradle/EstructurasDatos.java
+++ src/main/java/org/gradle/EstructurasDatos.java
@@ -76,53 +76,183 @@ public class EstructurasDatos {
 	}
 
 	public int evaluarCambioAPositivo(int nro1) {
-		
-		/*if (nro1 >= 0) {
-			return nro1;
-			
-		} else {
-			return nro1*-1;
-		}*/
-		
+
+		/*
+		 * if (nro1 >= 0) { return nro1;
+		 * 
+		 * } else { return nro1*-1; }
+		 */
+
 		return Math.abs(nro1);
-		
+
 	}
 
 	public boolean evaluarTablasDeVerdadSi(boolean variable_1, boolean variable_2) {
-		
+
 		if (variable_1 && variable_2) {
 			return true;
-		}
-		else
+		} else
 			return false;
-		
+
 	}
 
 	public boolean evaluarTablasDeVerdadOr(boolean variable_1, boolean variable_2) {
-		
-		if (variable_1||variable_2) {
-			 return true;
+
+		if (variable_1 || variable_2) {
+			return true;
 		} else {
 			return false;
 		}
-		
+
 	}
 
 	public boolean evaluarTablasDeVerdadNegacion(boolean variable_1) {
-		// TODO Auto-generated method stub
+
 		return !variable_1;
 	}
 
 	public String definirFestividad(String color_1, String color_2) {
-		
-		if (color_1=="Rojo" && color_2=="Verde") {
+
+		if (color_1 == "Rojo" && color_2 == "Verde") {
 			return "Navidad";
-		} else if (color_1=="Verde" && color_2=="Naranja") {
+		} else if (color_1 == "Verde" && color_2 == "Naranja") {
 			return "Halloween";
-		}else {
+		} else {
 			return "Pascua";
 		}
-		
+
 	}
 
+	public boolean definirUltimo(int int1, int int2) {
+		return ((int2 % 10) == int1);
+	}
+
+	public int definirMayor(int int1, int int2, int int3) {
+		// TODO Auto-generated method stub
+
+		/*
+		 * if (int1 > int2) { if (int1 > int3) { return int1; } } else { if (int2 >
+		 * int3) { return int2; }
+		 * 
+		 * } return int3;
+		 */
+		return (Math.max(int1, Math.max(int2, int3)));
+	}
+
+	public int multiplicarSumando(int int1, int int2) {
+		// TODO Auto-generated method stub
+
+		// int contador=0;
+		int resultado = 0;
+		/*
+		 * while (contador < int2) { resultado = resultado + int1; contador++; }
+		 */
+
+		for (int i = 0; i < int2; i++) {
+			resultado = resultado + int1;
+		}
+		return resultado;
+	}
+
+	public int potencia(int base, int potencia) {
+		int contador = 0;
+		int resultado = base;
+
+		/*
+		 * while (contador < potencia-1) { int mult=0; int contint=0; while (contint <
+		 * base ) { mult=mult+resultado; contint++; } contador++; resultado=mult; }
+		 */
+		// Refactorin
+		while (contador < potencia - 1) {
+			resultado = multiplicarSumando(resultado, base);
+			contador++;
+		}
+
+		return resultado;
+	}
+
+	public int potenciaPow(int int1, int int2) {
+
+		return (int) Math.pow(int1, int2);
+	}
+
+	public int ArrayElementoMaximo(int[] intArreglo) {
+		// TODO Auto-generated method stub
+		int maximo = 0;
+		for (int i = 0; i < intArreglo.length; i++) {
+
+			/*
+			 * if (intArreglo[i] > maximo) { maximo=intArreglo[i]; }
+			 */
+
+			maximo = Math.max(maximo, intArreglo[i]);
+
+		}
+		return maximo;
+	}
+
+	public boolean ArrayElementoSeis(int[] intArreglo) {
+
+		return ((intArreglo[0] == 6) || (intArreglo[(intArreglo.length - 1)] == 6));
+
+	}
+
+	public int[] ArrayMasGrande(int[] intArreglo1, int[] intArreglo2) {
+		// TODO Auto-generated method stub
+		if ((intArreglo1[0] + intArreglo1[1]) > (intArreglo2[0] + intArreglo2[1])) {
+			return intArreglo1;
+		} else {
+			return intArreglo2;
+		}
+
+	}
+
+	public int[] ArrayConstruccion(int intContador) {
+
+		int[] resultado = new int[intContador];
+
+		for (int i = 0; i < intContador; i++) {
+			resultado[i] = i;
+		}
+
+		return resultado;
+	}
+
+	public int ArrayMultiplicacion(int[] arreglo) {
+
+		return multiplicarSumando(arreglo[0], arreglo[1]);
+	}
+
+	public int Factorial(int num1) {
+		
+		if (num1 != 1) {
+			//Obtenerlo con multiplicacion
+			//return  num1 * Factorial(num1 - 1);
+			
+			//Obteniendolo con sumas
+			return multiplicarSumando(num1, Factorial(num1 - 1));
+		} else {
+			return 1;
+		}
+	}
+
+	public int SumatoriaRecursiva(int num1) {
+		// TODO Auto-generated method stub
+		
+		if (num1==0) {
+			return 0;
+			
+		}else {
+			if ((num1 % 2) == 0) {
+				return 3 + SumatoriaRecursiva(num1 -1); 
+			}else {
+				return 2 + SumatoriaRecursiva(num1 -1);
+			}
+		}
+			
+	}
 }
+
+
+
+
--- src/test/java/org/gradle/EstructurasDatosTest.java
+++ src/test/java/org/gradle/EstructurasDatosTest.java
@@ -381,7 +381,7 @@ public class EstructurasDatosTest {
 		// assert
 		assertFalse(resultado);
 	}
-	
+
 	@Test
 	public void ingresoPositivo5yObtengoPositivoTest() {
 		// arrange definicion de la variables
@@ -394,7 +394,7 @@ public class EstructurasDatosTest {
 		// assert
 		assertTrue(resultado);
 	}
-	
+
 	@Test
 	public void ingresoNegativo3yObtengoNegativoTest() {
 		// arrange definicion de la variables
@@ -407,7 +407,7 @@ public class EstructurasDatosTest {
 		// assert
 		assertFalse(resultado);
 	}
-	
+
 	@Test
 	public void ingresoPositivo2yObtengoPositivoTest() {
 		// arrange definicion de la variables
@@ -420,7 +420,7 @@ public class EstructurasDatosTest {
 		// assert
 		assertTrue(resultado);
 	}
-	
+
 	@Test
 	public void ingresoNegativo5yObtengoNegativoTest() {
 		// arrange definicion de la variables
@@ -433,7 +433,7 @@ public class EstructurasDatosTest {
 		// assert
 		assertFalse(resultado);
 	}
-	
+
 	@Test
 	public void ingresoPositivo0yObtengoPositivoTest() {
 		// arrange definicion de la variables
@@ -446,7 +446,7 @@ public class EstructurasDatosTest {
 		// assert
 		assertTrue(resultado);
 	}
-	
+
 	@Test
 	public void ingresoNumero2yObtengoParTest() {
 		// arrange definicion de la variables
@@ -459,7 +459,7 @@ public class EstructurasDatosTest {
 		// assert
 		assertTrue(resultado);
 	}
-	
+
 	@Test
 	public void ingresoNumero3yObtengoImparTest() {
 		// arrange definicion de la variables
@@ -472,7 +472,7 @@ public class EstructurasDatosTest {
 		// assert
 		assertFalse(resultado);
 	}
-	
+
 	@Test
 	public void ingresoNumero4yObtengoParTest() {
 		// arrange definicion de la variables
@@ -485,7 +485,7 @@ public class EstructurasDatosTest {
 		// assert
 		assertTrue(resultado);
 	}
-	
+
 	@Test
 	public void ingresoNumero5yObtengoImparTest() {
 		// arrange definicion de la variables
@@ -498,7 +498,7 @@ public class EstructurasDatosTest {
 		// assert
 		assertFalse(resultado);
 	}
-	
+
 	@Test
 	public void ingresoNumero2yObtengo2Test() {
 		// arrange definicion de la variables
@@ -511,7 +511,7 @@ public class EstructurasDatosTest {
 		// assert
 		assertEquals(2, resultado);
 	}
-	
+
 	@Test
 	public void ingresoNumeroNegativo4yObtengo4Test() {
 		// arrange definicion de la variables
@@ -524,7 +524,7 @@ public class EstructurasDatosTest {
 		// assert
 		assertEquals(4, resultado);
 	}
-	
+
 	@Test
 	public void ingresoNumeroNegativo6yObtengo6Test() {
 		// arrange definicion de la variables
@@ -537,7 +537,7 @@ public class EstructurasDatosTest {
 		// assert
 		assertEquals(6, resultado);
 	}
-	
+
 	@Test
 	public void ingresoNumeroPositivo7yObtengo7Test() {
 		// arrange definicion de la variables
@@ -550,7 +550,7 @@ public class EstructurasDatosTest {
 		// assert
 		assertEquals(7, resultado);
 	}
-	
+
 	@Test
 	public void ingresoTrueTrueyTrueTest() {
 		// arrange definicion de la variables
@@ -559,12 +559,12 @@ public class EstructurasDatosTest {
 
 		// act- accion
 
-		boolean resultado = estructuraDeDatos.evaluarTablasDeVerdadSi(variable_1,variable_2);
+		boolean resultado = estructuraDeDatos.evaluarTablasDeVerdadSi(variable_1, variable_2);
 
 		// assert
 		assertTrue(resultado);
 	}
-	
+
 	@Test
 	public void ingresoTrueFalseyObtengoFalseTest() {
 		// arrange definicion de la variables
@@ -573,12 +573,12 @@ public class EstructurasDatosTest {
 
 		// act- accion
 
-		boolean resultado = estructuraDeDatos.evaluarTablasDeVerdadSi(variable_1,variable_2);
+		boolean resultado = estructuraDeDatos.evaluarTablasDeVerdadSi(variable_1, variable_2);
 
 		// assert
 		assertFalse(resultado);
 	}
-	
+
 	@Test
 	public void ingresoFalseTrueyObtengoFalseTest() {
 		// arrange definicion de la variables
@@ -587,12 +587,12 @@ public class EstructurasDatosTest {
 
 		// act- accion
 
-		boolean resultado = estructuraDeDatos.evaluarTablasDeVerdadSi(variable_1,variable_2);
+		boolean resultado = estructuraDeDatos.evaluarTablasDeVerdadSi(variable_1, variable_2);
 
 		// assert
 		assertFalse(resultado);
 	}
-	
+
 	@Test
 	public void ingresoFalseFalseyObtengoFalseTest() {
 		// arrange definicion de la variables
@@ -601,12 +601,12 @@ public class EstructurasDatosTest {
 
 		// act- accion
 
-		boolean resultado = estructuraDeDatos.evaluarTablasDeVerdadSi(variable_1,variable_2);
+		boolean resultado = estructuraDeDatos.evaluarTablasDeVerdadSi(variable_1, variable_2);
 
 		// assert
 		assertFalse(resultado);
 	}
-	
+
 	@Test
 	public void ingresoTrueTrueyObtengoTrueTest() {
 		// arrange definicion de la variables
@@ -615,12 +615,12 @@ public class EstructurasDatosTest {
 
 		// act- accion
 
-		boolean resultado = estructuraDeDatos.evaluarTablasDeVerdadOr(variable_1,variable_2);
+		boolean resultado = estructuraDeDatos.evaluarTablasDeVerdadOr(variable_1, variable_2);
 
 		// assert
 		assertTrue(resultado);
 	}
-	
+
 	@Test
 	public void ingresoTrueFalseyObtengoTrueTest() {
 		// arrange definicion de la variables
@@ -629,12 +629,12 @@ public class EstructurasDatosTest {
 
 		// act- accion
 
-		boolean resultado = estructuraDeDatos.evaluarTablasDeVerdadOr(variable_1,variable_2);
+		boolean resultado = estructuraDeDatos.evaluarTablasDeVerdadOr(variable_1, variable_2);
 
 		// assert
 		assertTrue(resultado);
 	}
-	
+
 	@Test
 	public void ingresoFalseTrueyObtengoTrueTest() {
 		// arrange definicion de la variables
@@ -643,12 +643,12 @@ public class EstructurasDatosTest {
 
 		// act- accion
 
-		boolean resultado = estructuraDeDatos.evaluarTablasDeVerdadOr(variable_1,variable_2);
+		boolean resultado = estructuraDeDatos.evaluarTablasDeVerdadOr(variable_1, variable_2);
 
 		// assert
 		assertTrue(resultado);
 	}
-	
+
 	@Test
 	public void ingresoFalseFalseyObtengoFalse2Test() {
 		// arrange definicion de la variables
@@ -657,17 +657,16 @@ public class EstructurasDatosTest {
 
 		// act- accion
 
-		boolean resultado = estructuraDeDatos.evaluarTablasDeVerdadOr(variable_1,variable_2);
+		boolean resultado = estructuraDeDatos.evaluarTablasDeVerdadOr(variable_1, variable_2);
 
 		// assert
 		assertFalse(resultado);
 	}
-	
+
 	@Test
 	public void ingresoTrueyObtengoFalseTest() {
 		// arrange definicion de la variables
 		boolean variable_1 = true;
-		
 
 		// act- accion
 
@@ -676,12 +675,11 @@ public class EstructurasDatosTest {
 		// assert
 		assertFalse(resultado);
 	}
-	
+
 	@Test
 	public void ingresoFalseyObtengoTrueTest() {
 		// arrange definicion de la variables
 		boolean variable_1 = false;
-		
 
 		// act- accion
 
@@ -690,49 +688,585 @@ public class EstructurasDatosTest {
 		// assert
 		assertTrue(resultado);
 	}
-	
+
 	@Test
 	public void ingresoRojoyVerdeyObtengoNavidadTest() {
 		// arrange definicion de la variables
 		String color_1 = "Rojo";
 		String color_2 = "Verde";
-		
 
 		// act- accion
 
-		String resultado = estructuraDeDatos.definirFestividad(color_1,color_2);
+		String resultado = estructuraDeDatos.definirFestividad(color_1, color_2);
 
 		// assert
-		assertEquals("Navidad",resultado);
+		assertEquals("Navidad", resultado);
 	}
-	
+
 	@Test
 	public void ingresoVerdeyNaranjayObtengoHalloweenTest() {
 		// arrange definicion de la variables
 		String color_1 = "Verde";
 		String color_2 = "Naranja";
-		
 
 		// act- accion
 
-		String resultado = estructuraDeDatos.definirFestividad(color_1,color_2);
+		String resultado = estructuraDeDatos.definirFestividad(color_1, color_2);
 
 		// assert
-		assertEquals("Halloween",resultado);
+		assertEquals("Halloween", resultado);
 	}
-	
+
 	@Test
 	public void ingresoRojoyNaranjayObtengoPascuaTest() {
 		// arrange definicion de la variables
 		String color_1 = "Rojo";
 		String color_2 = "Naranja";
-		
 
 		// act- accion
 
-		String resultado = estructuraDeDatos.definirFestividad(color_1,color_2);
+		String resultado = estructuraDeDatos.definirFestividad(color_1, color_2);
 
 		// assert
-		assertEquals("Pascua",resultado);
+		assertEquals("Pascua", resultado);
+	}
+
+	@Test
+	public void ingreso8y18yObtengoTrue() {
+		// arrange definicion de la variables
+		int int1 = 8;
+		int int2 = 18;
+
+		// act- accion
+
+		boolean resultado = estructuraDeDatos.definirUltimo(int1, int2);
+
+		// assert
+		assertTrue(resultado);
+	}
+
+	@Test
+	public void ingreso3y113yObtengoTrue() {
+		// arrange definicion de la variables
+		int int1 = 3;
+		int int2 = 113;
+
+		// act- accion
+
+		boolean resultado = estructuraDeDatos.definirUltimo(int1, int2);
+
+		// assert
+		assertTrue(resultado);
+	}
+
+	@Test
+	public void ingreso6y117yObtengoTrue() {
+		// arrange definicion de la variables
+		int int1 = 6;
+		int int2 = 117;
+
+		// act- accion
+
+		boolean resultado = estructuraDeDatos.definirUltimo(int1, int2);
+
+		// assert
+		assertFalse(resultado);
+	}
+
+	@Test
+	public void ingreso8y18y30yObtengoTrue() {
+		// arrange definicion de la variables
+		int int1 = 8;
+		int int2 = 18;
+		int int3 = 30;
+
+		// act- accion
+
+		int resultado = estructuraDeDatos.definirMayor(int1, int2, int3);
+
+		// assert
+		assertEquals(30, resultado);
+	}
+
+	@Test
+	public void ingreso29y5y15yObtengoTrue() {
+		// arrange definicion de la variables
+		int int1 = 29;
+		int int2 = 5;
+		int int3 = 15;
+
+		// act- accion
+
+		int resultado = estructuraDeDatos.definirMayor(int1, int2, int3);
+
+		// assert
+		assertEquals(29, resultado);
+	}
+
+	@Test
+	public void ingreso10y50y5yObtengoTrue() {
+		// arrange definicion de la variables
+		int int1 = 10;
+		int int2 = 50;
+		int int3 = 5;
+
+		// act- accion
+
+		int resultado = estructuraDeDatos.definirMayor(int1, int2, int3);
+
+		// assert
+		assertEquals(50, resultado);
+	}
+
+	@Test
+	public void ingreso2y2yObtengo4() {
+		// arrange definicion de la variables
+		int int1 = 2;
+		int int2 = 2;
+
+		// act- accion
+
+		int resultado = estructuraDeDatos.multiplicarSumando(int1, int2);
+
+		// assert
+		assertEquals(4, resultado);
+	}
+
+	@Test
+	public void ingreso3y4yObtengo12() {
+		// arrange definicion de la variables
+		int int1 = 3;
+		int int2 = 4;
+
+		// act- accion
+
+		int resultado = estructuraDeDatos.multiplicarSumando(int1, int2);
+
+		// assert
+		assertEquals(12, resultado);
+	}
+
+	@Test
+	public void ingreso5y5yObtengo25() {
+		// arrange definicion de la variables
+		int int1 = 5;
+		int int2 = 5;
+
+		// act- accion
+
+		int resultado = estructuraDeDatos.multiplicarSumando(int1, int2);
+
+		// assert
+		assertEquals(25, resultado);
+	}
+
+	@Test
+	public void ingreso2y2yObtengoPotencia4() {
+		// arrange definicion de la variables
+		int int1 = 2;
+		int int2 = 2;
+
+		// act- accion
+
+		int resultado = estructuraDeDatos.potencia(int1, int2);
+
+		// assert
+		assertEquals(4, resultado);
+	}
+
+	@Test
+	public void ingreso3y4yObtengoPotencia81() {
+		// arrange definicion de la variables
+		int int1 = 3;
+		int int2 = 4;
+
+		// act- accion
+
+		int resultado = estructuraDeDatos.potencia(int1, int2);
+
+		// assert
+		assertEquals(81, resultado);
+	}
+
+	@Test
+	public void ingreso5y5yObtengoPotencia3125() {
+		// arrange definicion de la variables
+		int base = 5;
+		int potencia = 5;
+
+		// act- accion
+
+		int resultado = estructuraDeDatos.potencia(base, potencia);
+
+		// assert
+		assertEquals(3125, resultado);
+	}
+
+	@Test
+	public void ingreso2y2yObtengoPotencia4Pow() {
+		// arrange definicion de la variables
+		int int1 = 2;
+		int int2 = 2;
+
+		// act- accion
+
+		int resultado = estructuraDeDatos.potenciaPow(int1, int2);
+
+		// assert
+		assertEquals(4, resultado);
+	}
+
+	@Test
+	public void ingreso3y4yObtengoPotencia81Pow() {
+		// arrange definicion de la variables
+		int int1 = 3;
+		int int2 = 4;
+
+		// act- accion
+
+		int resultado = estructuraDeDatos.potenciaPow(int1, int2);
+
+		// assert
+		assertEquals(81, resultado);
+	}
+
+	@Test
+	public void ingreso5y5yObtengoPotencia3125Pow() {
+		// arrange definicion de la variables
+		int int1 = 5;
+		int int2 = 5;
+
+		// act- accion
+
+		int resultado = estructuraDeDatos.potenciaPow(int1, int2);
+
+		// assert
+		assertEquals(3125, resultado);
+	}
+
+	@Test
+	public void ingreso5y3y8y1yObtengoMayor8() {
+		// arrange definicion de la variables
+		int[] intArreglo = { 5, 3, 8, 1 };
+
+		// act- accion
+
+		int resultado = estructuraDeDatos.ArrayElementoMaximo(intArreglo);
+
+		// assert
+		assertEquals(8, resultado);
+	}
+
+	@Test
+	public void ingreso2y3y2y1yObtengoMayor3() {
+		// arrange definicion de la variables
+		int[] intArreglo = { 2, 3, 2, 1 };
+
+		// act- accion
+
+		int resultado = estructuraDeDatos.ArrayElementoMaximo(intArreglo);
+
+		// assert
+		assertEquals(3, resultado);
+	}
+
+	@Test
+	public void ingreso15y4y14yNeg5yObtengoMayor15() {
+		// arrange definicion de la variables
+		int[] intArreglo = { 15, 4, 14, -5 };
+
+		// act- accion
+
+		int resultado = estructuraDeDatos.ArrayElementoMaximo(intArreglo);
+
+		// assert
+		assertEquals(15, resultado);
+	}
+	
+	@Test
+	public void ingreso6y3y8y1yObtengoTrue() {
+		// arrange definicion de la variables
+		int[] intArreglo = { 6, 3, 8, 1 };
+
+		// act- accion
+
+		boolean resultado = estructuraDeDatos.ArrayElementoSeis(intArreglo);
+
+		// assert
+		assertTrue(resultado);
+	}
+	
+	@Test
+	public void ingreso2y3y2y6yObtengoTrue() {
+		// arrange definicion de la variables
+		int[] intArreglo = { 2, 3, 2, 6 };
+
+		// act- accion
+
+		boolean resultado = estructuraDeDatos.ArrayElementoSeis(intArreglo);
+
+		// assert
+		assertTrue(resultado);
+	}
+	
+	@Test
+	public void ingreso6y4y14y6yObtengoTrue() {
+		// arrange definicion de la variables
+		int[] intArreglo = { 6, 4, 14, 6 };
+
+		// act- accion
+
+		boolean resultado = estructuraDeDatos.ArrayElementoSeis(intArreglo);
+
+		// assert
+		assertTrue(resultado);
+	}
+	
+	@Test
+	public void ingreso5y4y2y1yObtengoFalse() {
+		// arrange definicion de la variables
+		int[] intArreglo = { 5, 4, 2, 1 };
+
+		// act- accion
+
+		boolean resultado = estructuraDeDatos.ArrayElementoSeis(intArreglo);
+
+		// assert
+		assertFalse(resultado);
+	}
+	
+	@Test
+	public void ingresoArreglo6y3Arreglo9y1yObtengoArreglo() {
+		// arrange definicion de la variables
+		int[] intArreglo1 = { 6,3 };
+		int[] intArreglo2 = { 9,1 };
+
+		// act- accion
+
+		int[] resultado = estructuraDeDatos.ArrayMasGrande(intArreglo1, intArreglo2);
+
+		// assert
+		assertArrayEquals(intArreglo2,resultado);
+	}
+	
+	
+	@Test
+	public void ingresoArreglo2y3Arreglo2y6yObtengoArreglo() {
+		// arrange definicion de la variables
+		int[] intArreglo1 = { 2,3 };
+		int[] intArreglo2 = { 2,6 };
+
+		// act- accion
+
+		int[] resultado = estructuraDeDatos.ArrayMasGrande(intArreglo1, intArreglo2);
+
+		// assert
+		assertArrayEquals(intArreglo2,resultado);
+	}
+	
+	@Test
+	public void ingresoArreglo6y4Arreglo1y6yObtengoArreglo() {
+		// arrange definicion de la variables
+		int[] intArreglo1 = { 6,4 };
+		int[] intArreglo2 = { 1,6 };
+
+		// act- accion
+
+		int[] resultado = estructuraDeDatos.ArrayMasGrande(intArreglo1, intArreglo2);
+
+		// assert
+		assertArrayEquals(intArreglo1,resultado);
+	}
+	
+	
+	@Test
+	public void ingresoTamano1yObtengoArreglo() {
+		// arrange definicion de la variables
+		int intContador = 1;
+		int [] arreglo= {0};
+
+		// act- accion
+
+		int[] resultado = estructuraDeDatos.ArrayConstruccion(intContador);
+
+		// assert
+		assertArrayEquals(arreglo,resultado);
+	}
+	
+	@Test
+	public void ingresoTamano2yObtengoArreglo() {
+		// arrange definicion de la variables
+		int intContador = 2;
+		int [] arreglo= {0,1};
+
+		// act- accion
+
+		int[] resultado = estructuraDeDatos.ArrayConstruccion(intContador);
+
+		// assert
+		assertArrayEquals(arreglo,resultado);
+	}
+	
+	
+	@Test
+	public void ingresoTamano3yObtengoArreglo() {
+		// arrange definicion de la variables
+		int intContador = 3;
+		int [] arreglo= {0,1,2};
+
+		// act- accion
+
+		int[] resultado = estructuraDeDatos.ArrayConstruccion(intContador);
+
+		// assert
+		assertArrayEquals(arreglo,resultado);
+	}
+	
+	@Test
+	public void ingresoTamano4yObtengoArreglo() {
+		// arrange definicion de la variables
+		int intContador = 4;
+		int [] arreglo= {0,1,2,3};
+
+		// act- accion
+
+		int[] resultado = estructuraDeDatos.ArrayConstruccion(intContador);
+
+		// assert
+		assertArrayEquals(arreglo,resultado);
+	}
+	
+	
+	@Test
+	public void ingresoArreglo2y2yObtengoMultiplicacionArreglo() {
+		// arrange definicion de la variables
+		int [] arreglo= {2,2};
+
+		// act- accion
+
+		int resultado = estructuraDeDatos.ArrayMultiplicacion(arreglo);
+
+		// assert
+		assertEquals(4,resultado);
+	}
+	
+	
+	@Test
+	public void ingresoArreglo3y4yObtengoMultiplicacionArreglo() {
+		// arrange definicion de la variables
+		int [] arreglo= {3,4};
+
+		// act- accion
+
+		int resultado = estructuraDeDatos.ArrayMultiplicacion(arreglo);
+
+		// assert
+		assertEquals(12,resultado);
+	}
+	
+	@Test
+	public void ingresoArreglo5y5yObtengoMultiplicacionArreglo() {
+		// arrange definicion de la variables
+		int [] arreglo= {5,5};
+
+		// act- accion
+
+		int resultado = estructuraDeDatos.ArrayMultiplicacion(arreglo);
+
+		// assert
+		assertEquals(25,resultado);
+	}
+	
+	@Test
+	public void ingresoArreglo1yObtengoFactorial() {
+		// arrange definicion de la variables
+		int num1= 1;
+
+		// act- accion
+
+		int resultado = estructuraDeDatos.Factorial(num1);
+
+		// assert
+		assertEquals(1,resultado);
+	}
+	
+	
+	@Test
+	public void ingresoArreglo2yObtengoFactorial() {
+		// arrange definicion de la variables
+		int num1= 2;
+
+		// act- accion
+
+		int resultado = estructuraDeDatos.Factorial(num1);
+
+		// assert
+		assertEquals(2,resultado);
+	}
+	
+	
+	@Test
+	public void ingresoArreglo3yObtengoFactorial() {
+		// arrange definicion de la variables
+		int num1= 3;
+
+		// act- accion
+
+		int resultado = estructuraDeDatos.Factorial(num1);
+
+		// assert
+		assertEquals(6,resultado);
+	}
+	
+	@Test
+	public void ingresoNumero0yObtengoSumatoriaRecursiva() {
+		// arrange definicion de la variables
+		int num1= 0;
+
+		// act- accion
+
+		int resultado = estructuraDeDatos.SumatoriaRecursiva(num1);
+
+		// assert
+		assertEquals(0,resultado);
+	}
+	
+	@Test
+	public void ingresoNumero1yObtengoSumatoriaRecursiva() {
+		// arrange definicion de la variables
+		int num1= 1;
+
+		// act- accion
+
+		int resultado = estructuraDeDatos.SumatoriaRecursiva(num1);
+
+		// assert
+		assertEquals(2,resultado);
+	}
+	
+	@Test
+	public void ingresoNumero2yObtengoSumatoriaRecursiva() {
+		// arrange definicion de la variables
+		int num1= 2;
+
+		// act- accion
+
+		int resultado = estructuraDeDatos.SumatoriaRecursiva(num1);
+
+		// assert
+		assertEquals(5,resultado);
+	}
+	
+	@Test
+	public void ingresoNumero4yObtengoSumatoriaRecursiva() {
+		// arrange definicion de la variables
+		int num1= 4;
+
+		// act- accion
+
+		int resultado = estructuraDeDatos.SumatoriaRecursiva(num1);
+
+		// assert
+		assertEquals(10,resultado);
 	}
 }
